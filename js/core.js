c7f686a3b1b7337e3b39f3044971492a66d26bf6
HABIB <118568528+weblearnerhabib@users.noreply.github.com>
2023-05-14 08:00:54 +0600

    Back to top Button Added
    Back to top Button Added

--- js/core.js
+++ js/core.js
@@ -22,4 +22,18 @@ $(function () {
     $(".Preloader").addClass("activatePreloader");
   });
 
+  // Back to top Scroll event
+  $(window).scroll(function () {
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 200) $('.go-top').fadeIn('slow');
+    if (scrolled < 200) $('.go-top').fadeOut('slow');
+  });
+
+  // Back to top event
+  $('.go-top').click(function () {
+    $("html, body").animate({
+      scrollTop: "0"
+    }, 500);
+  });
+
 });
